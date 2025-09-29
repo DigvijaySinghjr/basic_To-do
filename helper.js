2bd9132c9422289768b7db458cdb85babb161998
DigvijaySinghjr <neymar9838jr@gmail.com>
2025-09-29 22:56:17 +0530

    refactor index.js and fix addAssociateNotes

---
+++ helper.js
@@ -0,0 +1,28 @@
+import NoteRepository from './src/repository/note-repository.js';
+import UserRepository from './src/repository/user-repository.js';
+
+const noteRepository = new NoteRepository();
+const userRepository = new UserRepository();
+
+
+export async function addAssociateNotes(userId, noteId) {
+        const user = await userRepository.get(userId);
+
+        if (!user) {                         //if user not foud
+            console.error('no user found');
+            return;
+        }
+        
+
+        // if not present
+        if (!user.associateNotes.includes(noteId)) {
+            user.associateNotes.push(noteId);
+
+            await userRepository.update(userId, { associateNotes: user.associateNotes });
+        } else {
+            console.log('this noteId is already present in associateNote field or this user');
+        }
+        return user.associateNotes;
+}
+
+
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import connect from './config/db_config.js';
 
+import { addAssociateNotes } from '../helper.js';
+
 import NoteRepository from './repository/note-repository.js';
 import UserRepository from './repository/user-repository.js';
 
@@ -37,8 +39,12 @@ app.post('/addNotes', async (req, res) => {       // creating notes
     const note = await noteRepository.create({
         content: req.body.content,
         //contributors: req.body.contributors
-        owner: req.body.owner                   
+        owner: req.body.userId                   
     });
+
+    // Add the note to the user's associateNotes list
+    addAssociateNotes(req.body.userId, note.id);                 //id of note is genrated just above
+
     console.log('no response was send');
     res.json(note);
 })
@@ -56,6 +62,10 @@ app.patch('/addContributor', async (req, res) => {      // we'll get user's id f
 
             // Update the note's contributors array in the database
             await noteRepository.update(req.body.noteId, { contributors: note.contributors });
+
+            // Add the note to the user's associateNotes list
+            addAssociateNotes(req.body.userId, req.body.noteId);
+
         } else {
             console.log('this user is already present as contributor');
         }
@@ -74,18 +84,8 @@ app.patch('/addContributor', async (req, res) => {      // we'll get user's id f
 
 app.patch('/addAssociateNotes', async(req, res) => {   
     try {
-        const user = await userRepository.get(req.body.userId);
-
-        // if not present
-        if (!user.associateNotes.includes(req.body.noteId)) {
-            user.associateNotes.push(req.body.noteId);
-
-            await userRepository.update(req.body.userId, { associateNotes: user.associateNotes });
-        } else {
-            console.log('this noteId is already present in associateNote field or this user');
-        }
-
-        res.json({ associateNotes: user.associateNotes });
+        const associateNotes = await addAssociateNotes(req.body.userId, req.body.noteId)     //calling helper func
+        res.json( {associateNotes} );
     } catch (error) {
         console.error('Error fetching note:', error);
         res.status(500).json({ error: 'Failed to fetch note' });
